c606db1be976f8219469a028858b87dc2ae61dee
Ivan Khomych <ivan.khomychh@gmail.com>
2025-12-09 19:56:12 +0200

    Create views for models interaction endpoints via different class-based views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,31 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    MovieViewSet,
+    CinemaHallList,
+    CinemaHallDetail,
+    ActorList,
+    ActorDetail,
+    GenreList,
+    GenreDetail,
+)
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
+    path("cinema-halls/", CinemaHallList.as_view(), name="cinema-hall-list"),
+    path(
+        "cinema-halls/<int:pk>/",
+        CinemaHallDetail.as_view(),
+        name="cinema_hall-detail"
+    ),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
 ]
 
 app_name = "cinema"
